a7370c41d59492b571983d0d707b8b00535ab9eb
HanZawNyein <hanzawnyineonline@gmail.com>
2023-01-12 20:39:09 +0630

    add pagination views

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 - Customizing how models are displayed
 - Working with QuerySets and managers
 - Function Based Views
+
+### Enhancing Your Blog with Advanced Features
+- Using canonical URLs for models
+- Creating SEO-friendly URLs for posts (unique_for_date attribute)
+- Modifying the URL patterns
+- Modifying the views
+- Modifying the canonical URL for posts
+- Adding pagination
+- Handling pagination errors
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 from .models import Post
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,'blog/post/list.html',{'posts':posts})
 
-def post_detail(request,id):
+def post_list(request):
+    post_list = Post.published.all()
+    paginatior = Paginator(post_list,1)
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginatior.page(page_number)
+    except EmptyPage:
+        posts = paginatior.page(paginatior.num_pages)
+    except PageNotAnInteger:
+        posts = paginatior.page(1)
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
     # try:
-        # post = Post.published.get(id=id)
-    post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+    # post = Post.published.get(id=id)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             publish__year=year, publish__month=month, publish__day=day, slug=post)
     # except Post.DoesNotExist:
-        # raise Http404("No Post Found.")
-    return render(request,'blog/post/detail.html',{'post':post})
+    # raise Http404("No Post Found.")
+    return render(request, 'blog/post/detail.html', {'post': post})
